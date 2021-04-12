54bed82ec8e7f4e309bf90dd542fe74702b6c011
emmalforman <73502597+emmalforman@users.noreply.github.com>
2021-04-12 15:16:25 -0700

    update html

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request #, render_template
+from flask import Blueprint, request, render_template
 
 home_routes = Blueprint("home_routes", __name__)
 
@@ -6,12 +6,12 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
+    return render_template("about.html")
 
 @home_routes.route("/hello")
 def hello_world():
@@ -20,4 +20,5 @@ def hello_world():
     # ... which will return None instead of throwing an error if key is not present
     # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
     name = request.args.get("name") or "World"
-    return f"Hello, {name}!"
+    message = f"Hello, {name}!"
+    return render_template("hello.html", message=message)
